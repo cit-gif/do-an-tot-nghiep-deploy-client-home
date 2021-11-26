88c559797fcbd682e7640c13b40f0ae4defae068
cit-git <dsba.19i@cit.udn.vn>
2021-11-26 16:26:14 +0700

    d

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,6 +4,9 @@ import { serverApi } from "./constrant";
 const instance = axios.create({
 	baseURL: serverApi,
 	withCredentials: true,
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+	},
 });
 
 export default instance;
